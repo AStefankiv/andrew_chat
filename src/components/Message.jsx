416b6cb093ec3ec6fe2450059cecf4681ba2925b
AStefankiv <stefankif35@gmail.com>
2024-08-21 21:50:21 -0600

    Start setup Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import user_andrew from "../img/user_andrew.png";
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className="message">
+      <div className="messageInfo">
+      <img src={user_andrew} alt="" />
+      <span>just now</span>
+        <div className="messageContent">
+          <p>Hey, how are you?</p>
+          <img src={user_andrew} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Message;
+//40 minutes
