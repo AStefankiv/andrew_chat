3bb2622fcd40a24228d3b8e3ef1ec24b24ce047f
AStefankiv <stefankif35@gmail.com>
2024-09-01 17:38:41 -0600

    Create firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtvIXLGHK1s_hHudltXhFgHN2PSwbgvtA",
+  authDomain: "chat-da355.firebaseapp.com",
+  projectId: "chat-da355",
+  storageBucket: "chat-da355.appspot.com",
+  messagingSenderId: "335685579154",
+  appId: "1:335685579154:web:36521f148d5fbff486914b",
+  measurementId: "G-1HB04QXHK0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+const analytics = getAnalytics(app);
