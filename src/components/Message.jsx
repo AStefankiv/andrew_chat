db8ddd7afd49aaf69afcc6af3e278ddce2d4a1ee
AStefankiv <stefankif35@gmail.com>
2024-09-01 09:28:58 -0600

    Adjust messageContent

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,18 +3,17 @@ import user_andrew from "../img/user_andrew.png";
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
-      <img src={user_andrew} alt="" />
-      <span>just now</span>
-        <div className="messageContent">
+        <img src={user_andrew} alt="" />
+        <span>just now</span>
+        </div>
+      <div className="messageContent">
           <p>Hey, how are you?</p>
           <img src={user_andrew} alt="" />
-        </div>
       </div>
     </div>
   )
 }
 
 export default Message;
-//40 minutes
