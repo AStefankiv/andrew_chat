1512140b6888c1d0e52a5ad16aa0b1c5cee04a4e
AStefankiv <stefankif35@gmail.com>
2024-09-03 21:29:04 -0600

    Add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,30 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./style.scss"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
 
   const {currentUser} = useContext(AuthContext);
+  
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser){
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
 
-  console.log("Current User:", currentUser);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/">
+          <Route index element={<ProtectedRoute>
+            <Home />
+          </ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import Add from "../img/add.png";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/login");
+    } catch (error) {
+      setErr(true);
+    }
+  }
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Realtime Chat</span>
-        <span className="title">Register</span>
-        <form>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
 
