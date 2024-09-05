97dd36cd6add6ab5c905315ffef9c58b8e14307c
AStefankiv <stefankif35@gmail.com>
2024-09-04 21:44:15 -0600

    Add useStates to Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = () => {};
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  }
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user" />
+        <input type="text" placeholder="Find a user" onKeyDown={handleKey} onChange={(e)=>setUsername(e.target.value)} />
         </div>
         <div className="userChat">
           <img src="https://hackspirit.com/wp-content/uploads/2021/06/Copy-of-Rustic-Female-Teen-Magazine-Cover.jpg" alt="user" />
