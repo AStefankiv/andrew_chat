74e7faacfb7c4d2d772259ac4671da6384677da0
AStefankiv <stefankif35@gmail.com>
2024-09-05 21:30:29 -0600

    Setup chat between 2 users

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -27,13 +30,34 @@ const Search = () => {
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   }
+
+  const handleSelect = async () => {
+    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    try {
+      const res = await getDocs(db, "chats", combinedId);
+      if (!res.exists()) {
+        await setDoc(doc (db, "chats", combinedId), {messages: []});
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="search">
       <div className="searchForm">
         <input type="text" placeholder="Find a user" onKeyDown={handleKey} onChange={(e)=>setUsername(e.target.value)} />
         </div>
         {err && <span>User not found!</span>}
-        {user && <div className="userChat">
+        {user && <div className="userChat" onClick={handleSelect}>
           <img src={user.photoURL} alt="user" />
         <div className="userChatInfo">
           <span>{user.displayName}</span>
